1e87b9d036c1fd8d3f4d09d95d21e865518e7443
GokdenizAkbal <akbalgokdeniz@gmail.com>
2025-08-12 14:54:24 +0300

    Revert changes on Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Make the app listen on port 80 inside the Docker container
-builder.WebHost.UseUrls("http://*:80");
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -12,19 +9,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    // app.UseHttpsRedirection(); // Disabled for Docker, since SSL is not configured inside the container
 }
 
-app.UseStaticFiles(); // Serve static content (CSS, JS, images)
-
+app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
 
-// Define default MVC route
+app.MapStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 
 app.Run();
